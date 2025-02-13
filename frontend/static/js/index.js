f9da7984be834c5a383d9a6338e6eb02261360b0
Rory Kane <roarigami@gmail.com>
2025-02-13 11:27:45 -0500

    static clientside page router

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,24 @@
- console.log("JS IS LOADED WOOOOH");
+
+const router = async () => {
+  const routes = [
+    { path: "/", view: () => console.log("Viewing Dashboard") },
+    { path: "/posts", view: () => console.log("Viewing Posts") },
+    { path: "/settings", view: () => console.log("Viewing Settins") },
+  ];
+
+
+  //Test each route for potentional match
+  const potentialMatches = routes.map(route => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path
+    };
+  });
+
+  console.log(potentialMatches);
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  router();
+});
